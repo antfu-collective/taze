d8a88b9828de83665cf30c949e4b2de1d825b837
Anthony Fu <anthonyfu117@hotmail.com>
2020-07-22 16:02:20 +0800

    feat: diff

--- src/check.ts
+++ src/check.ts
@@ -1,52 +1,24 @@
+import path from 'path'
 import chalk from 'chalk'
 import { loadDependencies } from './in/load-dependencies'
 import { checkUpdates } from './in/check-updates'
-import { columnLog } from './log'
+import { columnLog, colorizeDiff } from './log'
 
 export async function check(argv: any) {
-  const { pkg, deps } = await loadDependencies('./package.json')
+  const filepath = path.resolve('./package.json')
+  const { pkg, deps } = await loadDependencies(filepath)
   const name = pkg.name
 
   await checkUpdates(deps)
-  console.log(chalk.cyan(name))
+  console.log(`${chalk.cyan(name)} ${chalk.gray(filepath)}`)
   console.log()
-  await columnLog(3, (log) => {
-    deps.forEach(({ name, currentVersion, latestVersion = '' }) => log(name, colorizeDiff(currentVersion, latestVersion), latestVersion))
+  await columnLog(4, (log) => {
+    deps.forEach(({ name, currentVersion, latestVersion = '' }) => log(`  ${name}`, chalk.gray(currentVersion), chalk.gray('->'), colorizeDiff(currentVersion, `^${latestVersion}`)))
+  }, {
+    pending: 2,
+    align: ['left', 'right', 'right', 'right'],
   })
   console.log()
 
   //
-}
-
-function colorizeDiff(from: string, to: string) {
-  let leadingWildcard = ''
-
-  // separate out leading ^ or ~
-  if (/^[~^]/.test(to) && to[0] === from[0]) {
-    leadingWildcard = to[0]
-    to = to.slice(1)
-    from = from.slice(1)
-  }
-
-  // split into parts
-  const partsToColor = to.split('.')
-  const partsToCompare = from.split('.')
-
-  let i = partsToColor.findIndex((part, i) => part !== partsToCompare[i])
-  i = i >= 0 ? i : partsToColor.length
-
-  // major = red (or any change before 1.0.0)
-  // minor = cyan
-  // patch = green
-  const color = i === 0 || partsToColor[0] === '0' ? 'red'
-    : i === 1 ? 'cyan'
-      : 'green'
-
-  // if we are colorizing only part of the word, add a dot in the middle
-  const middot = i > 0 && i < partsToColor.length ? '.' : ''
-
-  return leadingWildcard
-        + partsToColor.slice(0, i).join('.')
-        + middot
-        + chalk[color](partsToColor.slice(i).join('.'))
 }
--- src/log.ts
+++ src/log.ts
@@ -1,5 +1,16 @@
-export async function columnLog(columns: number, fn: (log: (...args: string[]) => void) => void, pending = 2) {
+import chalk from 'chalk'
+
+interface Options {
+  pending: number
+  align: string[]
+}
+
+export async function columnLog(columns: number, fn: (log: (...args: string[]) => void) => void, options: Partial<Options> = {}) {
   const lines: string[][] = []
+  const {
+    pending = 2,
+    align = [],
+  } = options
 
   const log = (...args: string[]) => {
     lines.push(args)
@@ -11,12 +22,90 @@ export async function columnLog(columns: number, fn: (log: (...args: string[]) =
 
   lines.forEach((line) => {
     for (let i = 0; i < columns; i++)
-      columnsWidth[i] = Math.max(columnsWidth[i], (line[i] || '').length)
+      columnsWidth[i] = Math.max(columnsWidth[i], stringLength(line[i] || ''))
   })
 
   lines.forEach((line) => {
-    for (let i = 0; i < columns; i++)
-      process.stdout.write((line[i] || '').padEnd(columnsWidth[i] + pending))
+    for (let i = 0; i < columns; i++) {
+      const pad = align[i] === 'right' ? 'padStart' : 'padEnd'
+      const part = line[i] || ''
+      process.stdout.write(part[pad](columnsWidth[i] + pending - padDiff(part)))
+    }
     process.stdout.write('\n')
   })
+}
+
+export function colorizeDiff(from: string, to: string) {
+  let leadingWildcard = ''
+
+  // separate out leading ^ or ~
+  if (/^[~^]/.test(to) && to[0] === from[0]) {
+    leadingWildcard = to[0]
+    to = to.slice(1)
+    from = from.slice(1)
+  }
+
+  // split into parts
+  const partsToColor = to.split('.')
+  const partsToCompare = from.split('.')
+
+  let i = partsToColor.findIndex((part, i) => part !== partsToCompare[i])
+  i = i >= 0 ? i : partsToColor.length
+
+  // major = red (or any change before 1.0.0)
+  // minor = cyan
+  // patch = green
+  const color = i === 0 || partsToColor[0] === '0' ? 'red'
+    : i === 1 ? 'cyan'
+      : 'green'
+
+  // if we are colorizing only part of the word, add a dot in the middle
+  const middot = i > 0 && i < partsToColor.length ? '.' : ''
+
+  return chalk.grey(leadingWildcard)
+        + partsToColor.slice(0, i).join('.')
+        + middot
+        + chalk[color](partsToColor.slice(i).join('.')).trim()
+}
+
+const padDiff = (str: string) => stringLength(str) - str.length
+
+const ansiRegex = ({ onlyFirst = false } = {}) => {
+  const pattern = [
+    '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+    '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))',
+  ].join('|')
+
+  return new RegExp(pattern, onlyFirst ? undefined : 'g')
+}
+const stripAnsi = (str: string) => typeof str === 'string' ? str.replace(ansiRegex(), '') : str
+const stringLength = (str: string) => {
+  if (str === '')
+    return 0
+
+  str = stripAnsi(str)
+
+  let width = 0
+
+  for (let i = 0; i < str.length; i++) {
+    const code = str.codePointAt(i)
+    if (!code)
+      continue
+
+    // Ignore control characters
+    if (code <= 0x1F || (code >= 0x7F && code <= 0x9F))
+      continue
+
+    // Ignore combining characters
+    if (code >= 0x300 && code <= 0x36F)
+      continue
+
+    // Surrogates
+    if (code > 0xFFFF)
+      i++
+
+    width += 1
+  }
+
+  return width
 }
