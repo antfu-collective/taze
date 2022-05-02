7bce4ebd5154ade2f083b5bad764d415959b978d
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-03 00:56:49 +0800

    chore: cleanup

--- scripts/postpublish.ts
+++
@@ -1,7 +0,0 @@
-import { promises as fs } from 'fs'
-
-(async() => {
-  await fs.unlink('./package.json')
-  await fs.copyFile('./package.back.json', './package.json')
-  await fs.unlink('./package.back.json')
-})()
