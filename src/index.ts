e0b7b01db0f99aa729d738540dffbe676dfb22a8
riri <70067449+Daydreamer-riri@users.noreply.github.com>
2023-03-20 16:24:10 +0800

    fix: defineConfig type error (#60) (#61)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { CommonOptions } from './types'
+import type { CheckOptions } from './types'
 
 export { resolvePackage, resolveDependencies, resolveDependency } from './io/resolves'
 export { loadPackage, loadPackages, writePackage } from './io/packages'
@@ -6,6 +6,6 @@ export { dumpDependencies, parseDependencies } from './io/dependencies'
 export { CheckPackages } from './api/check'
 export * from './types'
 
-export function defineConfig(config: Partial<CommonOptions>) {
+export function defineConfig(config: Partial<CheckOptions>) {
   return config
 }
