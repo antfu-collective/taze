a2bbaceec1e0de4ccb63acd130b5008408af1032
Anthony Fu <anthonyfu117@hotmail.com>
2023-10-15 22:30:00 +0800

    fix: cache `getNpmConfig`

--- src/utils/npm.ts
+++ src/utils/npm.ts
@@ -2,10 +2,11 @@
 
 import path from 'node:path'
 import process from 'node:process'
-import NpmcliConfig from '@npmcli/config'
+import type { Recordable } from '@npmcli/config'
+import NpmCliConfig from '@npmcli/config'
 
-async function getNpmConfig() {
-  const npmcliConfig = new NpmcliConfig({
+async function _getNpmConfig() {
+  const npmcliConfig = new NpmCliConfig({
     definitions: {},
     npmPath: path.dirname(process.cwd()),
     flatten: (current, total) => {
@@ -36,4 +37,10 @@ async function getNpmConfig() {
   return npmcliConfig.flat
 }
 
-export { getNpmConfig }
+let _cache: Promise<Recordable> | undefined
+
+export function getNpmConfig() {
+  if (!_cache)
+    _cache = _getNpmConfig()
+  return _cache
+}
