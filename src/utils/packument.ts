ac935ad1eca83834d20927abf94bb9427ee20bfa
SukkaW <isukkaw@gmail.com>
2024-04-14 00:18:47 +0800

    fix: use `npa` to parse package name before fetch

--- src/utils/packument.ts
+++ src/utils/packument.ts
@@ -1,15 +1,23 @@
 import process from 'node:process'
 import npmRegistryFetch from 'npm-registry-fetch'
 import { joinURL } from 'ufo'
+import npa from 'npm-package-arg'
 
 import type { Packument, Options as PacoteOptions } from 'pacote'
 
-export async function fetchPackumentWithFullMetaData(name: string, opts: PacoteOptions): Promise<Packument> {
+export async function fetchPackumentWithFullMetaData(spec: string, opts: PacoteOptions): Promise<Packument> {
+  const { name } = npa(spec)
+
+  if (!name)
+    throw new Error(`Invalid package name: ${name}`)
+
   const registry = npmRegistryFetch.pickRegistry(name, opts)
+
   const url = joinURL(registry, name)
   const fetchOptions = {
     ...opts,
     headers: {
+      // ensure that we always send *something*, other wise npm registry will reject the request
       'user-agent': opts.userAgent || `taze@npm node/${process.version}`,
       // use `application/json` to fetch full metadata
       'accept': 'application/json',
