e7587bad8ed5589d2053002e3981d8f01b55a167
Anthony Fu <anthonyfu117@hotmail.com>
2024-04-13 18:46:11 +0200

    chore: lint

--- src/types.ts
+++ src/types.ts
@@ -28,12 +28,12 @@ export type DiffType = 'major' | 'minor' | 'patch' | 'error' | null
 // @types/pacote uses "import = require()" syntax which is not supported by unbuild
 // So instead of using @types/pacote, we declare the type definition with only fields we need
 export interface Packument {
-  name: string
+  'name': string
   /**
    * An object where each key is a version, and each value is the manifest for
    * that version.
    */
-  versions: Record<string, Omit<Packument, 'versions'>>
+  'versions': Record<string, Omit<Packument, 'versions'>>
   /**
    * An object mapping dist-tags to version numbers. This is how `foo@latest`
    * gets turned into `foo@1.2.3`.
@@ -43,7 +43,7 @@ export interface Packument {
    * In the full packument, an object mapping version numbers to publication
    * times, for the `opts.before` functionality.
    */
-  time: Record<string, string> & {
+  'time': Record<string, string> & {
     created: string
     modified: string
   }
