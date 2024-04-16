ed12bc6a2c9bacd835893b3589d3b34577e24af0
Sukka <isukkaw@gmail.com>
2024-04-16 19:30:38 +0800

    fix: change the build config to bundle deps correctly, fix #111 (#112)

--- build.config.ts
+++ build.config.ts
@@ -7,6 +7,17 @@ export default defineBuildConfig({
   ],
   rollup: {
     inlineDependencies: true,
+    json: {
+      compact: true,
+      namedExports: false,
+      preferConst: false,
+    },
+    commonjs: {
+      requireReturnsDefault: 'auto',
+    },
+    dts: {
+      respectExternal: false,
+    },
   },
   clean: true,
   declaration: true,
