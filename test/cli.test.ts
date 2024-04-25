bf603c51870a62635005a9cb26eda7f3adee34e1
SukkaW <isukkaw@gmail.com>
2024-04-26 01:31:03 +0800

    refactor: replace `execa` w/ `ezspawn`

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -1,11 +1,11 @@
 import path from 'node:path'
 import { expect, it } from 'vitest'
-import { execa } from 'execa'
+import { async as ezspawnAsync } from '@jsdevtools/ez-spawn'
 
 it('taze cli should just works', async () => {
   const binPath = path.resolve(__dirname, '../bin/taze.mjs')
 
-  const proc = await execa(process.execPath, [binPath], { stdio: 'pipe' })
+  const proc = await ezspawnAsync(process.execPath, [binPath], { stdio: 'pipe' })
 
   expect(proc.stderr).toBe('')
 })
