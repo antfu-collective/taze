32d893da695ac0300ad00d943622d38e967ecb19
SukkaW <isukkaw@gmail.com>
2024-04-26 01:49:08 +0800

    chore: rename alias `ezspawnAsync` -> `ezspawn`

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -1,11 +1,11 @@
 import path from 'node:path'
 import { expect, it } from 'vitest'
-import { async as ezspawnAsync } from '@jsdevtools/ez-spawn'
+import { async as ezspawn } from '@jsdevtools/ez-spawn'
 
 it('taze cli should just works', async () => {
   const binPath = path.resolve(__dirname, '../bin/taze.mjs')
 
-  const proc = await ezspawnAsync(process.execPath, [binPath], { stdio: 'pipe' })
+  const proc = await ezspawn(process.execPath, [binPath], { stdio: 'pipe' })
 
   expect(proc.stderr).toBe('')
 })
