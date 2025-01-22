a7574716fac9d02f338ebc464b5d55b4ed980ae8
Anthony Fu <github@antfu.me>
2025-01-22 12:16:20 +0800

    fix: vscode addon

--- src/addons/vscode.ts
+++ src/addons/vscode.ts
@@ -10,22 +10,23 @@ export const addonVSCode: Addon = {
       return
     }
 
-    const minEngineVersion = semver.minVersion(pkg.raw.engines.vscode)
-    if (!minEngineVersion) {
-      return
-    }
-
     const version: string = pkg.raw.dependencies?.['@types/vscode']
       || pkg.raw.devDependencies?.['@types/vscode']
       || pkg.raw.peerDependencies?.['@types/vscode']
       || ''
 
     // Protocols like `workspace:` and `catalog:`, we skip them
-    if (version.includes(':')) {
+    if (!version || version.includes(':')) {
       return
     }
 
-    if (version && semver.gt(version, minEngineVersion)) {
+    const minEngineVersion = semver.minVersion(pkg.raw.engines.vscode)
+    const minVersion = semver.minVersion(version)
+    if (!minEngineVersion || !minVersion) {
+      return
+    }
+
+    if (semver.gt(minVersion, minEngineVersion)) {
       // eslint-disable-next-line no-console
       console.log(`[taze addon] Updated VS Code engine field to ${version}`)
       // If the version is not a range (fixed version), we prepend it with a caret
