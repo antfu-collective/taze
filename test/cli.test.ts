1f96c474b3a0cb7b914cc3458ad7a3784e03d4c7
Anthony Fu <github@antfu.me>
2025-08-21 08:38:57 +0900

    chore: update tests

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -5,7 +5,8 @@ import { expect, it } from 'vitest'
 it('taze cli should just works', async () => {
   const binPath = resolve(__dirname, '../bin/taze.mjs')
 
-  const proc = await exec(process.execPath, [binPath], { throwOnError: true })
+  const proc = await exec(process.execPath, [binPath], { throwOnError: false })
 
   expect(proc.stderr).toBe('')
+  expect(proc.exitCode).toBe(0)
 })
