86b811274c22f4d4941fa8aa07ab93511ae3b27a
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-01-20 00:04:46 +0800

    update test

--- test/package-manager.test.ts
+++ test/package-manager.test.ts
@@ -8,6 +8,7 @@ function getPkgInfo(name: string, result: ResolvedDepChange[]) {
   return result.filter(r => r.name === name)[0]
 }
 
+const pnpmVersion = '10.23.0'
 const pnpmIntegrity = 'sha512-IcTlaYACrel+Tv6Li0qJqN48haN5GflX56DzDzj7xbvdBZgP/ikXmy+25uaRJC4JjZRdFgF3LK0P71+2QR4qSw=='
 const pnpmHexHash = '21c4e5698002ade97e4efe8b8b4a89a8de3c85a37919f957e7a0f30f38fbc5bbdd05980ffe29179b2fb6e6e691242e098d945d1601772cad0fef5fb6411e2a4b'
 
@@ -24,11 +25,12 @@ describe('check package for packageManager', async () => {
   }
   const result = (await CheckPackages(options, {})).packages[0].resolved
   const pnpmInfo = getPkgInfo('pnpm', result)
-  const version = pnpmInfo.currentVersion.slice(1)
 
   it('update pnpm packageManager', () => {
     expect(pnpmInfo.update).toBe(true)
     expect(pnpmInfo.hexHash).toBeUndefined()
+    expect(pnpmInfo.currentVersion).toBe(`^${pnpmVersion}`)
+    const version = pnpmInfo.currentVersion.slice(1)
     expect(pnpmInfo.pkgData.integrity?.[version]).toBe(pnpmIntegrity)
   })
 })
